3d00af6068b1c4ab4949b73bdeb6e1b27c3527e0
Matt <ddrlear@yahoo.com>
2022-02-14 21:50:08 -0600

    Added Instructions
    Added Instructions

--- tes3mp-server 0.8.0/server/scripts/custom/levelCap.lua
+++ tes3mp-server 0.8.0/server/scripts/custom/levelCap.lua
@@ -1,3 +1,21 @@
+--[[
+	Lear's Level Cap Script
+		version 1.00 (for TES3MP 0.8)
+	
+	DESCRIPTION:
+	This simple script allows server owners to set a level cap for players. Keep in mind, it does not retroactively 
+		revert players levels if they have bypassed said cap prior to installing this script.
+
+	INSTALLATION:
+		1) Place this file as `levelCap.lua` inside your TES3MP servers `server\scripts\custom` folder.
+		2) Open your `customScripts.lua` file in a text editor. 
+				(It can be found in `server\scripts` folder.)
+		3) Add the below line to your `customScripts.lua` file:
+				require("custom.levelCap")
+		4) BE SURE THERE IS NO `--` SYMBOLS TO THE LEFT OF IT, ELSE IT WILL NOT WORK.
+		5) Save `customScripts.lua` and restart your server.
+
+--]]
 
 local playerLevelCap = 100 -- Level cap value.
 
