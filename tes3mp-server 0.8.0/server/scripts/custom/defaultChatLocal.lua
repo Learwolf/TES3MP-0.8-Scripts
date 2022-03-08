dbd4b0e6fea011e23cc22f621d23276f9879cd86
Matt <ddrlear@yahoo.com>
2022-03-07 20:41:38 -0600

    Fixed a crash
    Fixed a crash.

--- tes3mp-server 0.8.0/server/scripts/custom/defaultChatLocal.lua
+++ tes3mp-server 0.8.0/server/scripts/custom/defaultChatLocal.lua
@@ -1,6 +1,6 @@
 --[[
 	Default to Local Chat
-		version 1.00 (For TES3MP 0.8)
+		version 1.01 (For TES3MP 0.8)
 			by Learwolf
 	
 	DESCRIPTION:
@@ -15,7 +15,10 @@
 				require("custom.defaultChatLocal")
 		4) BE SURE THERE IS NO `--` SYMBOLS TO THE LEFT OF IT, ELSE IT WILL NOT WORK.
 		5) Save `customScripts.lua` and restart your server.
-		
+	
+	VERSIONS:
+		1.01	- Fixed a crash.
+		1.00	- Per request, initial untested release.
 --]]
 
 
@@ -23,8 +26,7 @@ defaultChatLocal = {}
 
 -- CONFIGURATION:
 local globalChatCommand = "global" -- Command to use (followed by message text) to speak in global chat. (i.e. "global" == /global enter text here to speak globally.)
-local showStaffBadgeInLocalChat = true -- If true, will display staff member badges in local chat, similar to how global chat does.
-local showStaffBadgeInGlobalChat = true -- If true, will display staff member badges in global chat.
+
 
 -- -- -- -- -- -- -- -- -- -- -- -- 
 -- No touch beyond this point.
@@ -33,17 +35,15 @@ local showStaffBadgeInGlobalChat = true -- If true, will display staff member ba
 defaultChatLocal.globalMessage = function(pid, cmd)
 	local message = color.White .. logicHandler.GetChatName(pid) .. ": " .. tableHelper.concatenateFromIndex(cmd, 2) .. "\n"
 
-	if showStaffBadgeInGlobalChat == true then
-		-- Check for chat overrides that add extra text
-		if Players[pid]:IsServerStaff() then
+	-- Check for chat overrides that add extra text
+	if Players[pid]:IsServerStaff() then
 
-			if Players[pid]:IsServerOwner() then
-				message = config.rankColors.serverOwner .. "[Owner] " .. message
-			elseif Players[pid]:IsAdmin() then
-				message = config.rankColors.admin .. "[Admin] " .. message
-			elseif Players[pid]:IsModerator() then
-				message = config.rankColors.moderator .. "[Mod] " .. message
-			end
+		if Players[pid]:IsServerOwner() then
+			message = config.rankColors.serverOwner .. "[Owner] " .. message
+		elseif Players[pid]:IsAdmin() then
+			message = config.rankColors.admin .. "[Admin] " .. message
+		elseif Players[pid]:IsModerator() then
+			message = config.rankColors.moderator .. "[Mod] " .. message
 		end
 	end
 
@@ -54,40 +54,16 @@ customCommandHooks.registerCommand(globalChatCommand, defaultChatLocal.globalMes
 
 customEventHooks.registerValidator("OnPlayerSendMessage", function(eventStatus, pid, message)
 	
-	 -- Is this a chat command? If so, pass it over to the commandHandler
-	if message:sub(1, 1) == '/' then
-
-		local command = (message:sub(2, #message)):split(" ")
-		commandHandler.ProcessCommand(pid, command)
-	else
-		local cellDescription = Players[pid].data.location.cell
-		
-		if logicHandler.IsCellLoaded(cellDescription) == true then
-			for index, visitorPid in pairs(LoadedCells[cellDescription].visitors) do
-
-				local message = logicHandler.GetChatName(pid) .. " to local area: "
-				
-				if showStaffBadgeInLocalChat == true then
-					-- Check for chat overrides that add extra text
-					if Players[pid]:IsServerStaff() then
-
-						if Players[pid]:IsServerOwner() then
-							message = config.rankColors.serverOwner .. "[Owner] " .. message
-						elseif Players[pid]:IsAdmin() then
-							message = config.rankColors.admin .. "[Admin] " .. message
-						elseif Players[pid]:IsModerator() then
-							message = config.rankColors.moderator .. "[Mod] " .. message
-						end
-					end
-				end
-				
-				message = message .. tableHelper.concatenateFromIndex(cmd, 2) .. "\n"
-				tes3mp.SendMessage(visitorPid, message, false)
-			end
+	local isValid = eventStatus.validDefaultHandler
+	
+	if isValid ~= false then
+		if message:sub(1, 1) ~= '/' then
+			OnPlayerSendMessage(pid, "/local "..message)
+			isValid = false
 		end
-
-	end	
-	eventStatus.validDefaultHandler = false
+	end
+	
+	eventStatus.validDefaultHandler = isValid
 	return eventStatus
 end)
 
